9a577c62a617f0a236850fa4bb1f1d963e7fa866
mohhaidir <khoiruljmbr@gmail.com>
2020-04-17 00:24:54 +0700

    coba socket backend gagal

--- server/app.js
+++ server/app.js
@@ -4,6 +4,18 @@ const port = process.env.PORT || 3000
 const router = require('./routers')
 const errorHandler = require('./middlewares/errorHandler')
 
+// const http = require('http').createServer(app);
+// const io = require('socket.io')(http);
+
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+
+//   socket.on('startGame', (words) => {
+//     io.local.emit('gameOn', words)
+//     socket.broadcast.emit('', message)
+//   })
+// });
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
