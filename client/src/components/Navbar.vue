de34da894e55685f4e60e98bbdd1211ef571c77e
Fadhil Muhammad <bringitdown0@gmail.com>
2020-04-17 09:52:33 +0700

    fix logout

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,44 +1,54 @@
 <template>
-  <div>
-    <div v-if="!check">
-      <router-link to="/">Masuk</router-link>
-    </div>
-    <div v-else>
-      <router-link to="/play">[ Play ]</router-link> |
-      <a @click="logout">[ Change User ]</a>
-    </div>
+  <div id="app">
+    <b-navbar id="nav" type="is-light" spaced>
+      <template slot="brand">
+        <b-navbar-item>
+          <h1 class="title">LoMbA KeTikQ</h1>
+        </b-navbar-item>
+      </template>
+      <template slot="end" v-if="showLogout">
+        <b-navbar-item tag="router-link" :to="{ path: '/play' }">
+          Play
+        </b-navbar-item>
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <b-button @click="logout" type="is-danger" expanded outlined>
+              Log Out
+            </b-button>
+          </div>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Navbar',
-  data() {
-    return {
-      check: true,
-    };
-  },
-  created() {
-    this.checkData();
+  computed: {
+    showLogout: {
+      get() {
+        let check = this.$store.state.isIngame;
+        let uname = localStorage.getItem('username');
+
+        if (uname || check) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+      set() {
+        //
+      },
+    },
   },
   methods: {
     logout() {
       localStorage.clear();
+      this.showLogout = false;
       this.check = false;
       this.$router.push('/');
       this.$store.dispatch('resetState');
-    },
-    checkData() {
-      const state = this.$store.state.isIngame;
-      const uname = localStorage.getItem('uname');
-
-      if (state) {
-        this.check = true;
-      } else if (uname) {
-        this.check = true;
-      } else {
-        this.check = false;
-      }
     },
   },
 };
