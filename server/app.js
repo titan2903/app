949f1469ad87ff7f13513d6b91ba4a89237d8a21
titan2903 <titanioy98@gmail.com>
2020-04-17 14:38:29 +0700

    add new update

--- server/app.js
+++ server/app.js
@@ -1,56 +1,55 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const router = require('./routers')
-const errorHandler = require('./middlewares/errorHandler')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const router = require("./routers");
+const errorHandler = require("./middlewares/errorHandler");
+const cors = require("cors");
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const cors =  require('cors');
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+let users = []; //!ini di ini name dan score object
 
-let users = [] //!ini di ini name dan score object
+io.on("connection", socket => {
+  console.log("an user connected");
 
-io.on('connection', (socket) => {
-  console.log('an user connected');
-
-  socket.on('emitUser', (user)=>{
+  socket.on("emitUser", user => {
     let player = {
       name: user,
       score: 0
-    }
+    };
 
-    users.push(player)
-    io.emit('pemain', users)
-  })
+    users.push(player);
+    io.emit("pemain", users);
+  });
 
-  socket.on('total_score', (score) =>{
+  socket.on("total_score", score => {
     users.forEach(user => {
-      if(user.name === score.name){
-        user.score = score.score
+      if (user.name === score.name) {
+        user.score = score.score;
       }
     });
 
-    io.emit('pemain', users)
-  })
+    io.emit("pemain", users);
+  });
 
-  socket.on('startGame', (words) => {
-    io.emit('created', words)
-    socket.broadcast.emit('created', 'socket broadcast')
-  })
+  socket.on("startGame", words => {
+    io.emit("created", words);
+    socket.broadcast.emit("created", "socket broadcast");
+  });
 
-  socket.on('message', (data)=>{
-    socket.broadcast.emit('message', data)
-  })
+  socket.on("message", data => {
+    socket.broadcast.emit("message", data);
+  });
 });
 
-app.use(router)
-app.use(errorHandler)
-
+app.use(router);
+app.use(errorHandler);
 
 http.listen(port, () => {
-  console.log(`Server running on PORT : ${port} !!`)
-})
+  console.log(`Server running on PORT : ${port} !!`);
+});
