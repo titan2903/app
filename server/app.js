4aa97ef70698529802f68cf3b2f0515c4e5b8bf2
verypei <verrymariyanto@gmail.com>
2020-04-17 13:02:46 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,26 +3,33 @@ const app = express()
 const port = process.env.PORT || 3000
 const router = require('./routers')
 const errorHandler = require('./middlewares/errorHandler')
+const cors = require('cors')
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
+io.on('connection', (socket) => {
 
+  socket.on('created', (message) => {
+    socket.broadcast.emit('created', message)
+  })
+
+  // socket.on('notifyAmountPlayer', (name) => {
+  //   socket.broadcast.emit('announce', name)
+  // })
+
+  // socket.on('startGame', (obj) => {
+  //   io.local.emit('gameOn', obj)
+  // })
+})
+
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(router)
 app.use(errorHandler)
 
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-
-  socket.on('startGame', (words) => {
-    io.local.emit('gameOn', words)
-    socket.broadcast.emit('', message)
-  })
-});
-
 http.listen(port, () => {
   console.log(`Server running on PORT : ${port} !!`)
 })
+© 2020 GitHub, Inc.
