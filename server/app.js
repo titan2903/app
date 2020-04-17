cbe28fcc0e5f9a4bbf052b8fcd2d484b23e50088
verypei <verrymariyanto@gmail.com>
2020-04-17 08:20:28 +0700

    test

--- server/app.js
+++ server/app.js
@@ -4,17 +4,6 @@ const port = process.env.PORT || 3000
 const router = require('./routers')
 const errorHandler = require('./middlewares/errorHandler')
 
-// const http = require('http').createServer(app);
-// const io = require('socket.io')(http);
-
-// io.on('connection', (socket) => {
-//   console.log('a user connected');
-
-//   socket.on('startGame', (words) => {
-//     io.local.emit('gameOn', words)
-//     socket.broadcast.emit('', message)
-//   })
-// });
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -22,6 +11,18 @@ app.use(express.urlencoded({ extended: true }))
 app.use(router)
 app.use(errorHandler)
 
-app.listen(port, () => {
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('startGame', (words) => {
+    io.local.emit('gameOn', words)
+    socket.broadcast.emit('', message)
+  })
+});
+
+http.listen(port, () => {
   console.log(`Server running on PORT : ${port} !!`)
 })
